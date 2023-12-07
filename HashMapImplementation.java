913c15771a030cdaa04f0b399fbe03fbf74d2e2a
Swapnaroop <swapnaroop2001@gmail.com>
2023-12-07 13:14:48 -0500

    HashMap a

--- HashMapImplementation.java
+++ HashMapImplementation.java
@@ -1,9 +1,17 @@
+import java.util.HashMap;
+
 /**
  * HashMapImplementation
  */
 public class HashMapImplementation {
 
     public static void main(String[] args) {
-        
+        HashMap<String,Integer> a=new HashMap<String,Integer>();
+        a.put("Ram", 100);
+        a.put("Laxman", 100);
+        a.put("Bharat", 100);
+        a.put("Hanuman", 100);
+
+        System.out.println(a);
     }
 }
