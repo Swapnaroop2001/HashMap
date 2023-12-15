bee1612ebcb58121f7378ebe9b5779debd17c827
Swapnaroop <swapnaroop2001@gmail.com>
2023-12-15 16:36:44 -0500

    Unique words

--- HashMapImplementation.java
+++ HashMapImplementation.java
@@ -4,8 +4,18 @@ import java.util.HashMap;
  * HashMapImplementation
  */
 public class HashMapImplementation {
+    public static Integer countUniqueWords(String[] words) {
+    HashMap<String, Integer> Words = new HashMap<String,Integer>();
+    for (int i = 0; i < words.length; i++) {
+      Words.put(words[i],i);
+    }
+    int count=Words.keySet().size();
+    System.out.println(Words.keySet());
+    return count; 
+  }
 
     public static void main(String[] args) {
+
         HashMap<String,Integer> a=new HashMap<String,Integer>();
         a.put("Ram", 100);
         a.put("Laxman", 100);
