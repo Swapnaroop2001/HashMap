bea285916c402497212362bfc3b5761c0db0cadf
Swapnaroop <swapnaroop2001@gmail.com>
2023-12-17 17:47:17 -0500

    RomanToInt

--- HashMapImplementation.java
+++ HashMapImplementation.java
@@ -4,25 +4,46 @@ import java.util.HashMap;
  * HashMapImplementation
  */
 public class HashMapImplementation {
-    public static Integer countUniqueWords(String[] words) {
-    HashMap<String, Integer> Words = new HashMap<String,Integer>();
+  public static Integer countUniqueWords(String[] words) {
+    HashMap<String, Integer> Words = new HashMap<String, Integer>();
     for (int i = 0; i < words.length; i++) {
-      Words.put(words[i],i);
+      Words.put(words[i], i);
     }
-    int count=Words.keySet().size();
+    int count = Words.keySet().size();
     System.out.println(Words.keySet());
-    return count; 
+    return count;
   }
 
-    public static void main(String[] args) {
-
-        HashMap<String,Integer> a=new HashMap<String,Integer>();
-        a.put("Ram", 100);
-        a.put("Laxman", 100);
-        a.put("Bharat", 100);
-        a.put("Hanuman", 100);
-
-        System.out.println(a);
-        System.out.println(a);
+  public static int romanToInt(String s) {
+    int sum=0;
+    HashMap< Character,Integer> romans= new HashMap<Character,Integer>();
+    romans.put('I', 1);
+    romans.put('V', 5);
+    romans.put('X', 10);
+    romans.put('L',50);
+    romans.put('C', 100);
+    romans.put('D', 500);
+    romans.put('M', 1000);
+    sum=sum+romans.get(s.charAt(0));
+    for (int i =1; i<s.length(); i++) {
+      if (romans.get(s.charAt(i))>romans.get(s.charAt(i-1))) {
+        sum=sum+romans.get(s.charAt(i))-(romans.get(s.charAt(i-1))*2);
+      } else {
+        sum=sum+romans.get(s.charAt(i));
+      }
     }
+
+    return sum;
+  }
+  public static void main(String[] args) {
+
+    HashMap<String, Integer> a = new HashMap<String, Integer>();
+    a.put("Ram", 100);
+    a.put("Laxman", 100);
+    a.put("Bharat", 100);
+    a.put("Hanuman", 100);
+    // System.out.println(a.get("Laxman"));
+    // System.out.println(a);
+    System.out.println(romanToInt("XVII"));
+  }
 }
--- HashSetImplementation.java
+++ HashSetImplementation.java
@@ -1,13 +1,16 @@
 import java.util.HashSet;
 
 public class HashSetImplementation {
-    public int lengthOfLongestSubstring(String s) {
+    public static int lengthOfLongestSubstring(String s) {
        HashSet<Character> a = new HashSet<Character>();
        
         for (int i = 0; i < s.length(); i++) {
-            a.add(null);
+            a.add(s.charAt(i));
         }
         return a.size();
+    }
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcabc"));
     }
 
     
