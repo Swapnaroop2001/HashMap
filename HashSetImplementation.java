b8b0211700132d53dc7e96a19a867e3133c8bd46
Swapnaroop <swapnaroop2001@gmail.com>
2023-12-18 20:14:41 -0500

    printpairs(int arr[], int sum)

--- HashSetImplementation.java
+++ HashSetImplementation.java
@@ -9,9 +9,23 @@ public class HashSetImplementation {
         }
         return a.size();
     }
-    public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("abcabcabc"));
-    }
 
+    //Check if pair with given Sum exists in Array (Two Sum)
+    static Boolean printpairs(int arr[], int sum){
+        HashSet<Integer> a = new HashSet<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            int temp=sum+arr[i];
+            if (a.contains(temp)) {
+                return true;
+            }
+            a.add(arr[i]);
+        }  
+        return false;     
+    }
     
+    public static void main(String[] args) {
+        int []arr={0, -1, 2, -3, 1};
+        int k=4;
+        System.out.println(printpairs(arr, k));
+    }
 }
